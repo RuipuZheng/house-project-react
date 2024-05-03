5c696bcdd422a53add1e4cf355c836e23d286974
Ruipu <ruipuzheng@gmail.com>
2024-05-03 13:49:06 -0400

    sign in /up /forgot pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import "./App.css";
-import Home from "./Pages/Home";
-import ForgotPassword from "./Pages/ForgotPassword";
-import Offers from "./Pages/Offers";
-import Profile from "./Pages/Profile";
-import Signin from "./Pages/Signin";
-import Signup from "./Pages/Signup";
+import Home from "./pages/Home";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/offers" element={<Offers />} />
